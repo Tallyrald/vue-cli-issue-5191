046aa0cdf6359378fb95f9e92ca9a1949aa10324
CrypterEmerald <mpeter007@gmail.com>
2020-02-25 15:23:50 +0100

    Added eslintrc & related tsconfig project file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	extends: [
+		'plugin:vue/recommended',
+		'@vue/typescript',
+	],
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		extraFileExtensions: ['.vue'],
+		parser: '@typescript-eslint/parser',
+		project: ['./tsconfig.json', './tsconfig.eslint.json'],
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		tsconfigRootDir: __dirname,
+	},
+	plugins: [
+		'@typescript-eslint',
+		'vue',
+	],
+};
